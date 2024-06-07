eb08f36e607ffcd0fb29608adcde7a315997a2be
onestead-murakami <t-murakami@onestead.co.jp>
2024-06-07 13:23:10 +0900

    Leaflet サンプル地図に移動経路初期表示とフォーム反映を追加

--- WebContent/leaflet.jsp
+++ WebContent/leaflet.jsp
@@ -9,8 +9,22 @@
 <link rel="stylesheet" href="./style/leaflet.css" />
 </head>
 <body>
-
-<div id="leaflet">ここに地図が表示される</div>
+<main>
+	<form id="my_form" action="" method="post">
+		<label>ポイント１</label>
+		<input type="text" name="my_lat1" id="my_lat1">
+		<input type="text" name="my_lng1" id="my_lng1">
+		<label>ポイント２</label>
+		<input type="text" name="my_lat2" id="my_lat2">
+		<input type="text" name="my_lng2" id="my_lng2">
+		<label>経路</label>
+		<input type="text" name="my_route" id="my_route">
+		<label>距離</label>
+		<input type="text" name="my_distance" id="my_distance">
+		<input type="submit" name="my_submit" value="ボタン">
+	</form>
+	<div id="my_leaflet">ここに地図が表示される</div>
+</main>
 
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
--- WebContent/script/leaflet.js
+++ WebContent/script/leaflet.js
@@ -1,7 +1,7 @@
 //地図を作成して表示
-const leafletMap = L.map('leaflet', {
+const leafletMap = L.map('my_leaflet', {
     center: [35.62, 139.72],
-    zoom: 13,
+    zoom: 18,
     crs: L.CRS.EPSG3857,
     attributionControl: false,
     contextmenu: false,
@@ -15,21 +15,31 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(leafletMap);
 
 //ルートr探索処理の関数
-const leafletLine = function(startLat, startLng, goalLat, goalLng) {
+const leafletLine = function(startLat, startLng, goalLat, goalLng, callbackRroutesfound) {
 	const line = L.Routing.control({
 	    waypoints: [
 	        L.latLng(startLat, startLng),
 	        L.latLng(goalLat, goalLng)
 	    ],
 	    routeWhileDragging: false
+	}).on('routesfound', function(e) {
+		callbackRroutesfound(e);
 	});
 	line.addTo(leafletMap);
 	return line;
 };
 
 //すでに登録されているマーカーを表示（緯度経度はDBから読み込まれる？？）
-const marker1 = L.marker([35.63, 139.77]).addTo(leafletMap);
-const marker2 = L.marker([35.64, 139.78]).addTo(leafletMap);
+const marker1 = L.marker([35.61946, 139.72091]).addTo(leafletMap);
+const marker2 = L.marker([35.61917, 139.72155]).addTo(leafletMap);
+//すでに登録されている2つのマーカーでルートを表示（緯度経度はDBから読み込まれる？？）
+L.polyline([
+	[35.61946, 139.72091],
+	[35.61956, 139.72102],
+	[35.61938, 139.72129],
+	[35.61918, 139.72155],
+	[35.61917, 139.72155],
+],{ "color": "blue", "weight": 10, "opacity": 0.5}).addTo(leafletMap);
 
 //2つのマーカーでルート探索結果を表示
 const layers = [];
@@ -45,8 +55,37 @@ leafletMap.on('click', function(e) {
         // ピンとピンのルートを表示する
         const start = layers[0].getLatLng();
         const goal = layers[1].getLatLng();
-        const line = leafletLine(start.lat, start.lng, goal.lat, goal.lng).on('routesfound', function(e) {
-			console.log("移動距離は " + e.routes[0].summary.totalDistance + " mです。");
+        const line = leafletLine(start.lat, start.lng, goal.lat, goal.lng, function(e) {
+			//ここで経路情報をHTML上のフォームに書き込む
+
+			const myLat1 = document.getElementById("my_lat1");
+			myLat1.value = start.lat;
+			const myLng1 = document.getElementById("my_lng1");
+			myLng1.value = start.lng;
+
+			const myLat2 = document.getElementById("my_lat2");
+			myLat2.value = goal.lat;
+			const myLng2 = document.getElementById("my_lng2");
+			myLng2.value = goal.lng;
+
+			const routeLatLng = e.routes[0].coordinates;
+			const myRoute = document.getElementById("my_route");
+			//データベースに格納しやすいように単純な配列に変換してみる
+			const arrayPoint = [];
+			for (let i = 0; i < routeLatLng.length; i++) {
+				const latLng = routeLatLng[i];
+				const point = [latLng.lat, latLng.lng];
+				arrayPoint.push(point);
+			}
+			myRoute.value = arrayPoint.join(" - ");//緯度経度の組み合わせが後でわかるように区切り方を工夫しないといけない
+
+			const totalDistance = e.routes[0].summary.totalDistance;
+			const myDistance = document.getElementById("my_distance");
+			myDistance.value = totalDistance;
+
+			console.log("移動距離は " + totalDistance + " mです。");
+			console.log("移動経路は " + routeLatLng + " です。");
+
 		});
         layers.push(line);
 	} else {

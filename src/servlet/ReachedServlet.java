ae5f66f38320dad29acb00c0dcec048f58390dc1
plusdojo2024-tochimoto <tochimoto-kokoa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-10 16:29:09 +0900

    残りのサーブレット

---
+++ src/servlet/ReachedServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class ReachedServlet
+ */
+@WebServlet("/ReachedServlet")
+public class ReachedServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}

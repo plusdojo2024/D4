144345e3db01959c2c1be3380778d00cec2c74a0
plusdojo2024-tochimoto <tochimoto-kokoa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-10 16:16:57 +0900

    RForm

---
+++ src/servlet/RFormServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class RFormServlet
+ */
+@WebServlet("/RFormServlet")
+public class RFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}

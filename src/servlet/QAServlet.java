775359971a791d073326a64fed34804d2be5229f
plusdojo2024-tochimoto <tochimoto-kokoa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-11 13:04:00 +0900

    QListとQAサーブレット

--- src/servlet/QAServlet.java
+++ src/servlet/QAServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +20,19 @@ public class QAServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/*
+		もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/D4/LoginServlet");
+			return;
+		}
+		*/
+		
+		//質問一覧ページにフォワードする。
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/q_a.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	/**

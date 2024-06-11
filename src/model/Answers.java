3f7641ffd5e4cc811632707b474bf8b2635498a7
plusdojo2024-nakata <nakata-daiki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-11 13:49:20 +0900

    2024/06/11

--- src/model/Answers.java
+++ src/model/Answers.java
@@ -1,5 +1,58 @@
 package model;
+import java.io.Serializable;
 
-public class Answers {
+public class Answers implements Serializable{
+	private int answers_id;
+	private int question_id;
+	private String answer;
+	private int user_id;
 
+	//引数がないコンストラクタ
+	public Answers() {
+
+	}
+
+	//引数があるコンストラクタ
+	public Answers(int answers_id,int question_id,String answer,int user_id) {
+		super();
+		this.answers_id=answers_id;
+		this.question_id=question_id;
+		this.answer=answer;
+		this.user_id=user_id;
+
+	}
+
+	public int getAnswers_id() {
+		return answers_id;
+	}
+
+	public void setAnswers_id(int answers_id) {
+		this.answers_id = answers_id;
+	}
+
+	public int getQuestion_id() {
+		return question_id;
+	}
+
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 }
+
+
--- src/model/Questions.java
+++ src/model/Questions.java
@@ -1,5 +1,56 @@
 package model;
+import java.io.Serializable;
 
-public class Questions {
+public class Questions implements Serializable{
+	private int question_id;
+	private String question;
+	private int user_id;
+	private String judge;
 
+	//引数がないコンストラクタ
+	public Questions() {
+
+	}
+
+	//引数があるコンストラクタ
+		public Questions(int question_id,String question,int user_id,String judge) {
+			super();
+			this.question_id=question_id;
+			this.question=question;
+			this.user_id=user_id;
+			this.judge=judge;
+
+		}
+
+		public int getQuestion_id() {
+			return question_id;
+		}
+
+		public void setQuestion_id(int question_id) {
+			this.question_id = question_id;
+		}
+
+		public String getQuestion() {
+			return question;
+		}
+
+		public void setQuestion(String question) {
+			this.question = question;
+		}
+
+		public int getUser_id() {
+			return user_id;
+		}
+
+		public void setUser_id(int user_id) {
+			this.user_id = user_id;
+		}
+
+		public String getJudge() {
+			return judge;
+		}
+
+		public void setJudge(String judge) {
+			this.judge = judge;
+		}
 }

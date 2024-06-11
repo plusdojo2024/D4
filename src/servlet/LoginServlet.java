21c4dc280df3d36402e83e3dfa1c67dcce71d51a
plusdojo2024-kawaguchi <kawaguchi-sho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-11 16:00:53 +0900

    6/11

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -29,8 +29,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.sendRedirect("/D4/HomeServlet");
 	}
 
 }

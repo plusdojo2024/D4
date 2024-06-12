e48cb1d374e014faebe4c1ee9c1422f6d659b0e1
plusdojo2024-yamaguchi <yamaguchi-shoya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 10:23:22 +0900

    RequestsDAO

--- src/dao/RequestsDAO.java
+++ src/dao/RequestsDAO.java
@@ -1,5 +1,194 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Requests;
+
 public class RequestsDAO {
+
+	//投稿（sql準備・DB接続）
+	public boolean insert(Requests card) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+			// SQL文を準備する（AUTO_INCREMENTのNUMBER列にはNULLを指定する）
+			String sql = "INSERT INTO Bc VALUES (NULL, ?, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (card.getAddress_order() != null && !card.getAddress_order().equals("")) {
+				pStmt.setString(1, card.getAddress_order());
+			}
+			else {
+				pStmt.setString(1, "（未設定）");
+			}
+			if (card.getRequest() != null && !card.getRequest().equals("")) {
+				pStmt.setString(2, card.getRequest());
+			}
+			else {
+				pStmt.setString(2, "（未設定）");
+			}
+			if (card.getUsers_id() != 0) {
+				pStmt.setString(3, Integer.toString(card.getUsers_id()));
+			}
+			else {
+				pStmt.setString(3, "（未設定）");
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+
+	//一覧表示（全件表示）（sql準備・DB接続）
+	public List<Requests> select(Requests card) {
+		Connection conn = null;
+		List<Requests> cardList = new ArrayList<Requests>();
+
+		//System.out.println(card.getNumber());
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT * FROM Requests ORDER BY requests_id DESC ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Requests record = new Requests(
+				rs.getInt("requests_id"),
+				rs.getString("address_order"),
+				rs.getString("request"),
+				rs.getInt("users_id")
+				);
+				cardList.add(record);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return cardList;
+	}
+
+
+	//件数指定表示（ホーム）（sql準備・DB接続）
+	public List<Requests> select_home() {
+		Connection conn = null;
+		List<Requests> cardList = new ArrayList<Requests>();
+
+		//System.out.println(card.getNumber());
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT TOP 3 * FROM Requests ORDER BY requests_id DESC ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Requests record = new Requests(
+				rs.getInt("requests_id"),
+				rs.getString("address_order"),
+				rs.getString("request"),
+				rs.getInt("users_id")
+				);
+				cardList.add(record);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return cardList;
+	}
 
 }

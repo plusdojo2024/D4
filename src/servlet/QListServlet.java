d5d02a9e1a1f74eca22483856c3288d7a376082d
plusdojo2024-nakata <nakata-daiki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 13:03:07 +0900

    Update QListServlet.java

--- src/servlet/QListServlet.java
+++ src/servlet/QListServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.QuestionsDAO;
+import model.Questions;
 
 /**
  * Servlet implementation class QListServlet
@@ -28,7 +32,20 @@ public class QListServlet extends HttpServlet {
 			return;
 		}
 		*/
-		
+
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				/*
+				int questions_id = Integer.parseInt(request.getParameter("questions_id"));
+				String question = request.getParameter("question");
+				int users_id = Integer.parseInt(request.getParameter("users_id"));
+				String judge = request.getParameter("judge");
+				*/
+
+				QuestionsDAO rDao = new QuestionsDAO();
+				//List<Bc> cardList = bDao.select(new Bc(0, "", companyName, "", department, position, "", name, "", note));
+				List<Questions> requestsList = rDao.select();
+
 		//質問一覧ページにフォワードする。
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/q_list.jsp");
 		dispatcher.forward(request, response);

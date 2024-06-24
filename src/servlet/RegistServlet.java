9b097b6ae1b798766117aed9ca2402455a6dd7d7
plusdojo2024-yamaguchi <yamaguchi-shoya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-24 10:41:07 +0900

    regist完成

--- src/servlet/RegistServlet.java
+++ src/servlet/RegistServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.sql.Date;
 
 import javax.servlet.RequestDispatcher;
@@ -55,17 +57,34 @@ public class RegistServlet extends HttpServlet {
 		String mail = request.getParameter("mail");
 		String password = request.getParameter("password");
 
+		//取得したpasswordをSHA256ハッシュ値に変換
+		String pass = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256"); //任意サイズのデータを取得して固定長のハッシュ値を出力するMessageDigestクラス
+			byte[] digest = md.digest(password.getBytes(StandardCharsets.UTF_8)); //String型のpasswordをbyte型配列に変換(getBytes())してハッシュ値を計算(md.digest())
+			for (byte b : digest) {
+				String a = Integer.toHexString(0xff & b); //バイナリのハッシュ値を16進数に変換
+				if (a.length() == 1) {
+					pass += "0" + a; //1桁の時0をつける
+				} else {
+					pass += a;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 
 		// 登録処理を行う
 		UsersDAO UDao = new UsersDAO();
-		if (UDao.insert(new Users(0, mail, password, 0, new Date(10000000000L), 0))) {
+		if (UDao.insert(new Users(0, mail, pass, 0, new Date(10000000000L), 0))) {
 			// 登録成功
 			request.setAttribute("result",
-			new Result("ユーザーを登録しました。", "/D4/regist.jsp"));
+					new Result("ユーザーを登録しました。", "/D4/regist.jsp"));
 		}
 		else {												// 登録失敗
 			request.setAttribute("result",
-			new Result("予期しないエラーが発生しました。", "/D4/regist.jsp"));
+					new Result("予期しないエラーが発生しました。", "/D4/regist.jsp"));
 		}
 
 		// 登録ページにフォワードする
